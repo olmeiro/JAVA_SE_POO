550d49c5af4cb834d7d2b5e2b4024704fdc3020e
olmeiro <olme.orozco@gmail.com>
2023-02-19 11:15:04 -0500

    feat: static variables and methods

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import static UI.UIMenu.*;
+
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+
+        Doctor myDoctor = new Doctor("Alenjadra Lópex", "Cardiología");
+        myDoctor.showName();
+        myDoctor.showSpeciality();
+        myDoctor.showId();
+        System.out.println(Doctor.id);
+
+        Doctor otherDoctor = new Doctor("Sara Saldarriage");
+        otherDoctor.showName();
+        otherDoctor.showId();
+        System.out.println(Doctor.id);
+
+        //UIMenu.showMenu(); //Mejor guardamos el UI en un package y lo importamos ->
+        showMenu();  //Modularización
     }
 }
