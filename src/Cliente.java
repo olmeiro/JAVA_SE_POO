cbe5d122eba53201dddbbfceab0829a47d739b87
olmeiro <olme.orozco@gmail.com>
2023-03-02 10:57:40 -0500

    feat: equals and hashCode methods

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Cliente {
 
     private String name;
@@ -8,6 +10,19 @@ public class Cliente {
         this.name = name;
         this.n_cuenta = n_cuenta;
         this.saldo = saldo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cliente cliente = (Cliente) o;
+        return Objects.equals(n_cuenta, cliente.n_cuenta);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(n_cuenta);
     }
 
     public String getName() {
--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,10 @@ public class Main {
         Cliente cl2 = new Cliente("Rafael Nadal", "00002", 250000);
         Cliente cl3 = new Cliente("Sandra Bullock", "00003", 300000);
         Cliente cl4 = new Cliente("Julio Iglesias", "00004", 800000);
+        /*
+            Cl5 es una copia identica de cl4 en cuanto a datos no en cuanto a objetos por ello el método add lo agrega. (son objetos diferentes)
+        * */
+        Cliente cl5 = new Cliente("Julio Iglesias", "00004", 800000);
 
         /*
         collection: de objeto de tipo Cliente, que no se repiten, tiene operaciones de agregación y eliminacion, también operaciones de lectura. no hay necesidad de ordenación por el momento.
@@ -21,10 +25,34 @@ public class Main {
         clientesBanco.add(cl2);
         clientesBanco.add(cl3);
         clientesBanco.add(cl4);
+        clientesBanco.add(cl5);
 
         /*Recorriendo colección  con for - each: */
         for (Cliente cliente: clientesBanco) {
             System.out.println(cliente.getName() + " " + cliente.getN_cuenta() + " " + cliente.getSaldo());
         }
+
+        System.out.println();
+        //Usando hashCode y equals con la clase Book:
+        Book libro1 = new Book("Programación en JAVA", "Juan", 25);
+        Book libro2 = new Book("Programación en JAVA", "Juan", 25);
+
+        //hagamos libro1 = libro2
+        // libro1 = libro2;
+
+        System.out.println("libro1 = libro2: " + libro1.equals(libro2));
+
+        /*Solución para comparar por hashCode*/
+        if(libro1.equals(libro2)){
+            System.out.println("Es el mismo libro");
+            System.out.println(libro1.hashCode());
+            System.out.println(libro2.hashCode());
+        }else{
+            System.out.println("No es el mismo libro");
+            System.out.println(libro1.hashCode());
+            System.out.println(libro2.hashCode());
+        }
+
+
     }
 }
